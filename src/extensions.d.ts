7a0dfd35130c160d4275551a9f89a45d9f4eb973
Kyle Lam <k.h.lam0614@gmail.com>
2022-02-06 22:03:08 +0800

    add extension.d.ts

---
+++ src/extensions.d.ts
@@ -0,0 +1,46 @@
+// Language: typescript
+// Path: src\extensions.d.ts
+declare module '*.ttf' {
+	const content: string
+	export default content
+}
+
+declare module '*.otf' {
+	const content: string
+	export default content
+}
+
+declare module '*.svg' {
+	const content: string
+	export default content
+}
+
+declare module '*.png' {
+	const content: string
+	export default content
+}
+
+declare module '*.jpg' {
+	const content: string
+	export default content
+}
+
+declare module '*.jpeg' {
+	const content: string
+	export default content
+}
+
+declare module '*.gif' {
+	const content: string
+	export default content
+}
+
+declare module '*.bmp' {
+	const content: string
+	export default content
+}
+
+declare module '*.tiff' {
+	const content: string
+	export default content
+}
